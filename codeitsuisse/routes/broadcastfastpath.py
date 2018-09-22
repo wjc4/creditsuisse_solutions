3a75e2a264c5224b4575d9d6b26e732e3f539e76
AkaHitsuji <yang025@e.ntu.edu.sg>
2018-09-23 01:16:30 +0800

    returned proper answer

--- codeitsuisse/routes/broadcastfastpath.py
+++ codeitsuisse/routes/broadcastfastpath.py
@@ -4,10 +4,9 @@ from flask import request, jsonify
 
 from codeitsuisse import app
 
-count = 0
 
-@app.route('/broadcaster/most-connected-node', methods=['POST'])
-def eval_broadcast_connected():
+@app.route('/broadcaster/fastest-path', methods=['POST'])
+def eval_broadcast_fastpath():
     data = request.get_json()
     app.logger.info("data sent for evaluation {}".format(data))
     input_data = data.get("data")
@@ -47,28 +46,12 @@ def leastnodes(data):
 
     while visited:
         mother_node = find_mother(node, visited[0])
-        count_children(node,mother_node,visited)
-        answer.append((mother_node, returncount()))
+        answer.append(mother_node)
         # print(answer)
-        reset()
-
-    max_count = 0
-    for letter,num in answer:
-        if num>max_count:
-            max_count = num
-
-    most_connected = []
-    for letter,num in answer:
-        if num==max_count:
-            most_connected.append(letter)
-    # print(most_connected)
-    # print("before sort:",most_connected)
-    most_connected = sorted(most_connected)
-    print("after sort:",most_connected)
-    # most_connected = most_connected.sorted()
+        remove_children(node,mother_node,visited)
 
     result_string = {
-        "result": most_connected[0]
+        "result": answer
     }
 
     return result_string
@@ -83,37 +66,22 @@ def find_mother(node, node_to_check):
         # print("parent found! parent is:",node_to_check)
         return node_to_check
 
-def count_children(node, node_to_check, visited):
+def remove_children(node, node_to_check, visited):
     if "to" in node[node_to_check]:
         for children_node in node[node_to_check]["to"]:
             # print("visiting children:", children_node)
             if children_node not in visited:
                 pass
             else:
-                count_children(node, children_node, visited)
+                remove_children(node, children_node, visited)
         # print("popping",node_to_check)
         visited.pop(visited.index(node_to_check))
-        plusone()
     else:
         # print("this is the node to check",node_to_check)
         # print("this is visited: ",visited)
         if node_to_check in visited:
             # print("popping",node_to_check)
             visited.pop(visited.index(node_to_check))
-            plusone()
         else:
             # print(node_to_check,"not in loop")
             pass
-
-
-def plusone():
-    global count
-    count += 1
-
-def reset():
-    global count
-    count = 0
-
-def returncount():
-    global count
-    return count
