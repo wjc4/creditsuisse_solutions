23e7c225d130cd3430e59b334e765ab21a0a3b2e
AkaHitsuji <yang025@e.ntu.edu.sg>
2018-09-23 00:09:16 +0800

    fixed check in for loop

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -12,6 +12,7 @@ import codeitsuisse.routes.photogps
 import codeitsuisse.routes.hotel
 import codeitsuisse.routes.puzzle
 import codeitsuisse.routes.broadcastmessage
+import codeitsuisse.routes.broadcastconnected
 
 if 'DYNO' in os.environ:
     logFormatter = logging.Formatter("%(asctime)s [%(filename)s] [%(funcName)s] [%(lineno)d] [%(levelname)-5.5s]  %(message)s")
