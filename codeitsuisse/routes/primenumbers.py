53211453d48067b1a3a3e02d97311b21cf1561c4
pohyk123 <pohyk123@gmail.com>
2018-09-22 09:48:23 +0800

    prime numbers

--- codeitsuisse/routes/primenumbers.py
+++ codeitsuisse/routes/primenumbers.py
@@ -7,39 +7,39 @@ from codeitsuisse import app
 
 # logger = logging.getLogger(__name__)
 
-# Creates a list of prime numbers up to given number
-def prime_list(A):
-    primes = []
-    bool_a = [True for i in range(2,A+3)]
-    for i in range(2, int(math.sqrt(A))+1):
-        if bool_a[i]:
-            for j in range( i * i, A +1 , i):
-                if bool_a[j]:
-                    bool_a[j] = False;
-
-    for i in range(2,A):
-        if bool_a[i]:
-            primes.append(i)
-
-    return primes
-
-#Returns two prime numbers whose sum is equal to the given number
-def prime_sum(A):
-    answers = []
-
-    for i in (prime_list(A)):
-        for j in (prime_list(A)[::-1]):
-            if i + j == A:
-                answers.append((i,j))
-                break
-
-    return min(answers)
-
-@app.route('/prime-sum', methods=['POST'])
-def prime_sum_eval():
-    data = request.get_json()
+@app.route('/generate_primes', methods=['POST'])
+def evaluate_primes():
+    # JSON mode
+    data = request.get_json();
     app.logger.info("data sent for evaluation {}".format(data))
-    inputValue = data.get("input")
-    result = prime_sum(inputValue)
+    inputValue = data.get("input");
+
+    # Args Key Mode
+    # data = request.args
+    # inputValue = int(data.get('input'))
+
+    result = sum_of_primes(inputValue)
     app.logger.info("My result :{}".format(result))
-    return jsonify(result)
+
+    return jsonify(result);
+
+
+def prime_sieve(limit):
+    prime = [True] * limit
+    prime[0] = prime[1] = False
+
+    for i, is_prime in enumerate(prime):
+        if is_prime:
+            yield i
+            for n in range(i * i, limit, i):
+                prime[n] = False
+
+def sum_of_primes(n):
+    primes = list(prime_sieve(n))
+    # for fast `in` lookups:
+    primes_set = set(primes)
+    for a in primes:
+        b = n - a
+        if b in primes_set:
+            # this is the lexicographically smallest by design
+            return [a, b]
