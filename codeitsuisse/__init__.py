9974b4286af78f536f3f0354058bb3d40ad0bca0
AkaHitsuji <yang025@e.ntu.edu.sg>
2018-09-22 09:33:29 +0800

    prime-numbers completed

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -5,6 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 
 import codeitsuisse.routes.square
+import codeitsuisse.routes.primenumbers
 
 if 'DYNO' in os.environ:
     logFormatter = logging.Formatter("%(asctime)s [%(filename)s] [%(funcName)s] [%(lineno)d] [%(levelname)-5.5s]  %(message)s")
