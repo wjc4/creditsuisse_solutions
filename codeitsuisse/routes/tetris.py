56c65b5f8b166dbb233674c3ee4ce5bec0366805
pohyk123 <pohyk123@gmail.com>
2018-09-23 03:30:31 +0800

    Tetris 3

--- codeitsuisse/routes/tetris.py
+++ codeitsuisse/routes/tetris.py
@@ -23,53 +23,53 @@ def tetris_evaluate():
     #     actions.append(shift)
 
     # challenge 2
-    J_count = 0
-    L_count = 0
-    for i in range(n):
-        rotate = 0
-        shift = 0
-        if(seq[i]=="L"):
-            if(int(L_count/3)%2==0):
-                shift = L_count%3 * 2
-            else:
-                rotate = 2
-                shift = L_count%3 * 2
-            L_count += 1
-        else:
-            offset = 6
-            if(int(J_count/2)%2==0):
-                shift = (J_count%2 * 2) + offset
-            else:
-                rotate = 2
-                shift = (J_count%2 * 2) + offset
-            J_count+=1
-
-        if(rotate!=0):
-            actions.append(int(str(rotate)+str(shift)))
-        else:
-            actions.append(shift)
-
-    #challenge 3
-    # O_count = 0
-    # I_count = 0
+    # J_count = 0
+    # L_count = 0
     # for i in range(n):
     #     rotate = 0
     #     shift = 0
-    #     if(seq[i]=="O"):
-    #         shift = J_count%3 * 2
-    #         O_count += 1
+    #     if(seq[i]=="L"):
+    #         if(int(L_count/3)%2==0):
+    #             shift = L_count%3 * 2
+    #         else:
+    #             rotate = 2
+    #             shift = L_count%3 * 2
+    #         L_count += 1
     #     else:
     #         offset = 6
-    #         if(int(L_count/4)%2 ==0):
-    #             shift = (L_count%4 * 1) + offset
+    #         if(int(J_count/2)%2==0):
+    #             shift = (J_count%2 * 2) + offset
     #         else:
-    #             shift = (L_count%4 * 1) + offset
-    #         I_count+=1
+    #             rotate = 2
+    #             shift = (J_count%2 * 2) + offset
+    #         J_count+=1
     #
     #     if(rotate!=0):
     #         actions.append(int(str(rotate)+str(shift)))
     #     else:
     #         actions.append(shift)
+
+    #challenge 3
+    O_count = 0
+    I_count = 0
+    for i in range(n):
+        rotate = 0
+        shift = 0
+        if(seq[i]=="O"):
+            shift = O_count%3 * 2
+            O_count += 1
+        else:
+            offset = 6
+            if(int(I_count/4)%2 ==0):
+                shift = (I_count%4 * 1) + offset
+            else:
+                shift = (I_count%4 * 1) + offset
+            I_count+=1
+
+        if(rotate!=0):
+            actions.append(int(str(rotate)+str(shift)))
+        else:
+            actions.append(shift)
 
 
     print("Final actions are as follows: ",actions)
